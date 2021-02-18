a8410ad443319c97ab79aaa84054a90ac55d7a7b
Jorim <j.vanderwal@students.uu.nl>
2021-02-18 11:27:59 +0100

    Remove unnecessary pickups

---
+++ Assets/Scripts/HeartPickup.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartPickup : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerController.Instance.Lives >= 3)
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/ScorePickup.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScorePickup : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerController.Instance.ScoreMultiplier >= 8)
+            Destroy(gameObject);
+    }
+}
